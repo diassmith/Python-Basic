2b8de820466c23f375dc0877edcd4979ddcba2cc
Guilherme Dias <33097438+diassmith@users.noreply.github.com>
2022-05-01 02:15:12 -0300

    Dev (#2)
    * Basic Syntax
    
    * Math operators
    
    Co-authored-by: diassmith <diassmith>

---
+++ Math Operations.py
@@ -0,0 +1,47 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 2 . - Mathematical operators
+# MAGIC Operators are used to perform operations on variables and values:
+# MAGIC 
+# MAGIC Sum
+# MAGIC Subtraction
+# MAGIC Multiplication
+# MAGIC Division
+# MAGIC exponentiation
+# MAGIC Rest of Division
+# MAGIC floor division
+
+# COMMAND ----------
+
+#Sum
+100+15
+
+# COMMAND ----------
+
+#Subtration
+100-15
+
+# COMMAND ----------
+
+#Multiplication
+100*2
+
+# COMMAND ----------
+
+#Division
+100/2
+
+# COMMAND ----------
+
+#Exponential
+2**2
+
+# COMMAND ----------
+
+#Rest of Division 
+100%2
+
+# COMMAND ----------
+
+#floor division
+100//2

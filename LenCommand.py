659cabe9afebde2e3446c215936bae056df74a8d
diassmith <diassmith>
2022-05-01 19:59:47 +0000

    LenCommand

---
+++ LenCommand.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 8º Command Len
+# MAGIC 
+# MAGIC The len() function returns the number of elements of any list in Python, this is yet another built-in function available in any part of our code.
+
+# COMMAND ----------
+
+List = [20, 40, 60, 80, 100]
+Dictionary = { 'Name':'Dias', 'Idade':23, 'Data Analytics':True }
+Tuple = (100, 200)
+String = 'Hello world!'
+
+print( len(List) )
+print( len(Dictionary) )
+print( len(Tuple) )
+print( len(String) )

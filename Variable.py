f78546af3c9b5386e362c9e577e65ccb7bbaa308
diassmith <diassmith>
2022-05-01 05:17:14 +0000

    Variables

---
+++ Variable.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 3º Variables
+# MAGIC 
+# MAGIC Variables are containers for storing data values.
+# MAGIC 
+# MAGIC A variable is created the moment you assign a value to it
+
+# COMMAND ----------
+
+name="Dias"
+print(name)
+
+# COMMAND ----------
+
+#text variables
+
+var_text = "text"
+var_text2 = str("str_text") 
+
+print(var_text2)
+
+# COMMAND ----------
+
+#numeric variables
+
+var_int = 100
+var_int_int = int(100)
+
+print(var_int_int)
+
+# COMMAND ----------
+
+#float variables
+
+var_float = 100.50
+var_float_float = float(100.50)
+
+print(var_float_float)
+
+# COMMAND ----------
+
+#boolean variables
+
+var_bool_true = True
+var_bool_false = False
+
+print(var_bool_true, var_bool_false)

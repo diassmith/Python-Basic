de3759d4967cf8df61a21c3a15322bec047840b5
diassmith <diassmith>
2022-05-02 00:35:01 +0000

    Input

---
+++ InputCommand.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 11º Input Command
+# MAGIC 
+# MAGIC The input() function receives as a parameter a string that will be shown as an aid to the user, usually informing him what kind of data the program is expecting to receive.
+# MAGIC 
+# MAGIC input()
+
+# COMMAND ----------
+
+# input Command
+# Send a moment information
+Name = input('What\'s your name?')
+Age = input('How old are you? ')
+
+print('Your name is:', Name)
+print('Your age is:', Age)

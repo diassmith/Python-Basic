b29c0a9924bd1efc41ee14d822138cfaf39c8ace
diassmith <diassmith>
2022-05-02 01:17:34 +0000

    ComparisonOperators

---
+++ ComparisonOperators.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 12- Comparison Operators
+# MAGIC Comparison operators are used to compare two values:
+# MAGIC 
+# MAGIC '==' (Equals to )
+# MAGIC 
+# MAGIC '!=' (Different)
+# MAGIC 
+# MAGIC '>' (Greater)
+# MAGIC 
+# MAGIC '<' (Smaller)
+# MAGIC 
+# MAGIC '>=' (Greater equals)
+# MAGIC 
+# MAGIC '<=' (Smallest equals)
+
+# COMMAND ----------
+
+# Comparison Operators - Pt 1
+Equal = '=='
+Different = '!='
+Greater = '>'
+GreatEqual = '>='
+Smaller = '<'
+Smallest_Equal = '<='
+
+print('100 é maior que 70:', 100 > 70 )
+print('10 é menor que 70:', 100 < 70 )
+print('100 é igual 70:', 100 == 70 )
+print('100 é maior 100:', 100> 100 )
+print('100 é maior ou igual à 100:', 100 >= 100)
+print('100 é menor 100:', 100< 100)
+print('100 é menor ou igual à 100:', 100 <= 100 )
